7b55204a502f3b1f3405dfead22d0e40c71e4bca
papanda <ru3aah@gmail.com>
2025-12-25 02:25:29 +0300

    main updated

--- app/main.py
+++ app/main.py
@@ -1,7 +1,25 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from db.db import init_db, async_engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+    await async_engine.dispose()
+
+
+app = FastAPI(
+    title="AIBot",
+    version="0.1.0",
+    description="AIBot - Telegram bot for AI news",
+    lifespan=lifespan
+)
+
 
 @app.get("/")
-async def root():
+async def read_root():
     return {"message": "Hello World"}
